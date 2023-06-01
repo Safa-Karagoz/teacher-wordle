fcf2a9b0b14c11e29111372b7ded06285e3d33ed
Safa-Karagoz <78101103+Safa-Karagoz@users.noreply.github.com>
2023-05-31 22:28:45 -0400

    done

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import { GoogleOAuthProvider, GoogleLogin, googleLogout } from '@react-oauth/google';
 import { FcGoogle } from 'react-icons/fc';
 import { useNavigate } from 'react-router-dom';
@@ -7,70 +7,85 @@ import { resetCookie } from '../utils/login';
 
 import Typewriter from 'typewriter-effect';
 import "../css/SignIn.css"
-
+import * as scripts from "../utils/scripts"
 
 const SignIn = () => {
-    const clientId = process.env.REACT_APP_GOOGLE_OAUTH_CLIENT_ID
-    const navigate = useNavigate();
-    const [loginFailure, setLoginFailure] = useState(false)
-  
-    async function handleCallbackResponse(res) {
-        var date = new Date();
-        var userObject = jwt_decode(res.credential)
-    
-        if (userObject.email.endsWith("bergen.org")){
-          document.cookie = "google-oauth-assembly.session=" + res.credential + "; expires=" + date.setDate(date.getDate() + 1) + ";SameSite=lax"
-          setLoginFailure(false)
-          navigate('/ex')
-        } else {
-          resetCookie()
-          setLoginFailure(true)
-          googleLogout();
-        }
-      }
+  const clientId = process.env.REACT_APP_GOOGLE_OAUTH_CLIENT_ID
+  const navigate = useNavigate();
+  const [loginFailure, setLoginFailure] = useState(false)
+
+  async function handleCallbackResponse(res) {
+    var date = new Date();
+    var userObject = jwt_decode(res.credential)
+
+    if (userObject.email.endsWith("bergen.org")) {
+      document.cookie = "google-oauth-assembly.session=" + res.credential + "; expires=" + date.setDate(date.getDate() + 1) + ";SameSite=lax"
+      setLoginFailure(false)
+      navigate('/game')
+    } else {
+      resetCookie()
+      setLoginFailure(true)
+      googleLogout();
+    }
+  }
 
   return (
-    <div className='container'> 
-      <div className="title">
-        <Typewriter onInit={(typewriter) => { 
-                  typewriter
-                  .typeString("Teacherle")
-                  .start()
 
-                }}
+    
+    <div className='container'>
+      <div className="title">
+        <Typewriter onInit={(typewriter) => {
+          typewriter
+            .typeString("TEACHERLE")
+            .start()
+
+        }}
         />
       </div>
       <div className='subtitle'>
-        <Typewriter onInit={(typewriter) => { 
-                  typewriter
-                  .typeString("BCA Teacher Wordle")
-                  .start()
-                }}
+        <Typewriter onInit={(typewriter) => {
+          typewriter
+            .typeString("BCA Teacher Wordle")
+            .start()
+        }}
         />
       </div>
-      
-        <div className="login-button">
-            <GoogleOAuthProvider clientId={clientId}>
-                <GoogleLogin
-                    render={(renderProps) => (
-                    <button type="button" id="google-login" 
-                            onClick={renderProps.onClick}
-                            disabled={renderProps.disabled}>
-                        <FcGoogle/> Sign in with google
-                    </button>
-                    )}
-                    onSuccess={handleCallbackResponse}
-                    onFailure={() => console.log('could not login')}
-                    cookiePolicy="single_host_origin" />
-            </GoogleOAuthProvider>
-        </div>
 
-        {loginFailure ? 
-        <p className='failure-message'><br/>*Please login use your bergen.org email*</p> 
-      : null}
-      
+      <div>
+        <div className="login-button">
+          <GoogleOAuthProvider clientId={clientId}>
+            <GoogleLogin
+              render={(renderProps) => (
+                <button type="button" id="google-login"
+                  onClick={renderProps.onClick}
+                  disabled={renderProps.disabled}>
+                  <FcGoogle /> Sign in and play!
+                </button>
+              )}
+              onSuccess={handleCallbackResponse}
+              onFailure={() => console.log('could not login')}
+              cookiePolicy="single_host_origin" />
+          </GoogleOAuthProvider>
+        </div>
+      </div>
+
+
+      {loginFailure ?
+        <p className='failure-message'><br />*Please login use your bergen.org email*</p>
+        : null}
+
+
+      {/* <button onClick={scripts.getTodaysTeacher}>date?</button> */}
+
+      <div className='bottom-text'>
+        <div className='date'>{scripts.getTodaysDate()}</div>
+        <div> Day No.{scripts.getDayNumber()}</div>
+        <p>Built by Safa and Matt</p>
+      </div>
+
     </div>
-    
+
+
   )
 }
 
---
+++ src/utils/scripts.js
@@ -0,0 +1,31 @@
+import json from "../assets/teacherValues.json"
+
+export function submitTeacherGuess() {
+   
+}
+
+export function getTodaysTeacher() { 
+    var today = new Date(); 
+    var value  = (1 + today.getMonth()) * 100 + today.getDate()
+    json.forEach( (teacher) => {
+        if (teacher.id === value){
+            return teacher.name;
+        }
+    })
+    
+    return "Scott Lang";
+}
+
+export function getDayNumber(){ 
+    const oneDay = 24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
+    const firstDate = new Date(2023, 4, 22);
+    const secondDate = new Date();
+
+    return Math.round(Math.abs((firstDate - secondDate) / oneDay));
+}
+
+export function getTodaysDate() {
+    var today = new Date(); 
+    const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    return monthNames[today.getMonth()] + " " + today.getDate() + ", " + today.getFullYear(); 
+}
